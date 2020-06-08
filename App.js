9aac9446e7ec7ee997eaae47a4bd8c57bd8cfa93
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-06-08 19:46:46 -0300

    splash screen

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   View,
   Text,
 } from 'react-native';
 
+import SplashScreen from 'react-native-splash-screen';
+
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+
   return (
     <>
       <View>
